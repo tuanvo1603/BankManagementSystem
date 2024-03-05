923e05fb292d66e036c765da76d3ca1f4a011664
tuanvo1603 <tuan.vv210905@sis.hust.edu.vn>
2024-03-05 08:58:30 +0700

    Init money and account management service

---
+++ money-and-account-management-service/src/test/java/com/example/moneyandaccountmanagementservice/MoneyAndAccountManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moneyandaccountmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyAndAccountManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
