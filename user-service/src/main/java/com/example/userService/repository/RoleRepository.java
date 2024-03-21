c40f740fa351433b632252e172626df152ef0140
tuanvo1603 <117852084+tuanvo1603@users.noreply.github.com>
2024-03-21 16:35:50 +0700

    Delete user-service directory

--- user-service/src/main/java/com/example/userService/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.userService.repository;
-
-
-import com.example.userService.enitity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Long> {
-}
