4022e14169596a6a6e80043296fa74e60b5d4255
Hoàng <hoangnv2810@gmail.com>
2024-03-22 22:22:30 +0700

    refactor: remove autowired

--- currency-and-account-service/src/main/java/com/example/bank/api/FetchCurrencyApi.java
+++ currency-and-account-service/src/main/java/com/example/bank/api/FetchCurrencyApi.java
@@ -4,14 +4,15 @@ import com.example.bank.model.Currency;
 import com.example.bank.request.FetchCurrencyRequest;
 import com.example.bank.response.FetchCurrencyResponse;
 import com.example.bank.service.CurrencyService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class FetchCurrencyApi extends CommonApi<FetchCurrencyResponse, FetchCurrencyRequest>{
 
-    @Autowired
-    private CurrencyService currencyService;
+    private final CurrencyService currencyService;
 
     @Override
     public FetchCurrencyResponse execute(FetchCurrencyRequest request) {
