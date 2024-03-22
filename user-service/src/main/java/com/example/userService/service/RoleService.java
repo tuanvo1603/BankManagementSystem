973fffc1b4575a818b762a7d19bbac4c72d5445e
tuanvo1603 <117852084+tuanvo1603@users.noreply.github.com>
2024-03-22 22:34:40 +0700

    Delete user-service/src/main directory

--- user-service/src/main/java/com/example/userService/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.example.userService.service;
-
-import com.example.userService.enitity.Role;
-import com.example.userService.exception.RoleFoundException;
-
-public interface RoleService {
-    Role createRole(Role role) throws RoleFoundException;
-    String removeRole(Long roleId);
-}
