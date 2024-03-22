f29b5fbd9f99517a6bf6e52ad126d30cfe7a094e
tuanvo1603 <117852084+tuanvo1603@users.noreply.github.com>
2024-03-22 22:34:57 +0700

    Delete money-and-account-management-service/src/main/java/com/example/bank directory

--- money-and-account-management-service/src/main/java/com/example/bank/utils/ApiUrl.java
+++
@@ -1,9 +0,0 @@
-package com.example.bank.utils;
-
-public class ApiUrl {
-    public static final String TIME_API_URL = "https://timeapi.io/api/Time/current/zone?timeZone=";
-
-    public static final String AUTH_SERVICE_API_URL = "http://localhost:8088/";
-
-
-}
