6dcd779839068c9829cff2bf6610177205860bda
Hoàng <hoangnv2810@gmail.com>
2024-03-25 16:53:25 +0700

    refactor: rename path controller

--- transaction-service/src/main/java/com/example/bank/controller/TransactionController.java
+++ transaction-service/src/main/java/com/example/bank/controller/TransactionController.java
@@ -1,11 +1,9 @@
 package com.example.bank.controller;
 
 import com.example.bank.api.*;
-import com.example.bank.model.Account;
 import com.example.bank.request.*;
 import com.example.bank.response.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
@@ -13,18 +11,16 @@ import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 
 @RestController
-@RequestMapping("/v1/customer")
+@RequestMapping("/transaction")
 @RequiredArgsConstructor
-public class CustomerController {
+public class TransactionController {
 
     private final DrawMoneyApi drawMoneyApi;
     private final DepositApi depositApi;
     private final TransferApi transferApi;
-    private final CreateAccountApi createAccountApi;
-    private final DeleteAccountApi deleteAccountApi;
     private final TransactionApi transactionApi;
 
-    @PostMapping("/draw-money")
+    @PostMapping("/draw")
     public DrawMoneyResponse drawMoney(@RequestParam String sourceAccountNumber, @RequestParam BigDecimal money) {
         DrawMoneyRequest drawMoneyRequest = new DrawMoneyRequest(sourceAccountNumber, money);
         return drawMoneyApi.execute(drawMoneyRequest);
@@ -53,18 +49,5 @@ public class CustomerController {
                 .get(7);
         TransactionRequest transactionRequest = new TransactionRequest(userId, pageNumber);
         return transactionApi.execute(transactionRequest);
-    }
-
-    @PostMapping("/create-account")
-    public CreateAccountResponse createAccount(@RequestBody Account account) {
-//        throw new RuntimeException();
-        CreateAccountRequest createAccountRequest = new CreateAccountRequest(account);
-        return createAccountApi.execute(createAccountRequest);
-    }
-
-    @DeleteMapping("/delete-account")
-    public DeleteAccountResponse deleteAccount(@RequestParam("accountNumber") String accountNumber) {
-        DeleteAccountRequest deleteAccountRequest = new DeleteAccountRequest(accountNumber);
-        return deleteAccountApi.execute(deleteAccountRequest);
     }
 }
