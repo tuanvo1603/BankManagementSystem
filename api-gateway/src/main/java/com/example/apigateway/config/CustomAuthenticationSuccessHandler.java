086c12b63b98302c84dc07c60abb2754422117f7
Hoàng <hoangnv2810@gmail.com>
2024-03-26 10:06:40 +0700

    feat: redirect home after login success

---
+++ api-gateway/src/main/java/com/example/apigateway/config/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,20 @@
+package com.example.apigateway.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.server.DefaultServerRedirectStrategy;
+import org.springframework.security.web.server.ServerRedirectStrategy;
+import org.springframework.security.web.server.WebFilterExchange;
+import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+public class CustomAuthenticationSuccessHandler extends RedirectServerAuthenticationSuccessHandler {
+    private ServerRedirectStrategy redirectStrategy = new DefaultServerRedirectStrategy();
+    @Override
+    public Mono<Void> onAuthenticationSuccess(WebFilterExchange webFilterExchange, Authentication authentication) {
+        ServerWebExchange exchange = webFilterExchange.getExchange();
+        return this.redirectStrategy.sendRedirect(exchange, URI.create("/home"));
+    }
+
+}
